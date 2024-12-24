7943e26d3dcde6ca3365ec2c951d56b8ebaaf6fa
Danninon <danninon@gmail.com>
2024-12-24 18:25:31 +0200

    .env configuration enabled

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,11 +1,7 @@
-// import dotenv from 'dotenv';
-//
-// dotenv.config();
-
 export default {
-    apiBaseUrl:  "http://ec2-54-144-64-88.compute-1.amazonaws.com:4000", // API URL
-    socketBaseUrl: "http://ec2-54-144-64-88.compute-1.amazonaws.com:4000", // WebSocket URL
-    port:  4000, // Backend port
+    apiBaseUrl: import.meta.env.VITE_APIBASEURL || "http://ec2-54-144-64-88.compute-1.amazonaws.com:4000", // API URL from .env
+    socketBaseUrl: import.meta.env.VITE_SOCKETBASEURL || "http://ec2-54-144-64-88.compute-1.amazonaws.com:4000", // WebSocket URL from .env
+    port: import.meta.env.VITE_SERVER_PORT || 4000 // Backend port from .env
 };
 
 // export default {
