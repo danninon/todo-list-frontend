a5753d45a27b837aa8258ea3ba97659ad50904cf
Dan Inon <76201610+danninon@users.noreply.github.com>
2024-12-24 17:41:33 +0200

    Update default.ts
    changing configuration

--- src/config/default.ts
+++ src/config/default.ts
@@ -2,14 +2,14 @@
 //
 // dotenv.config();
 
-// export default {
-//     apiBaseUrl:  "http://ec2-54-144-64-88.compute-1.amazonaws.com:4000", // API URL
-//     socketBaseUrl: "http://ec2-54-144-64-88.compute-1.amazonaws.com:4000", // WebSocket URL
-//     port:  4000, // Backend port
-// };
-
 export default {
-    apiBaseUrl:  "http://localhost:4000", // API URL
-    socketBaseUrl: "http://localhost:4000", // WebSocket URL
+    apiBaseUrl:  "http://ec2-54-144-64-88.compute-1.amazonaws.com:4000", // API URL
+    socketBaseUrl: "http://ec2-54-144-64-88.compute-1.amazonaws.com:4000", // WebSocket URL
     port:  4000, // Backend port
 };
+
+// export default {
+//     apiBaseUrl:  "http://localhost:4000", // API URL
+//     socketBaseUrl: "http://localhost:4000", // WebSocket URL
+//     port:  4000, // Backend port
+// };
